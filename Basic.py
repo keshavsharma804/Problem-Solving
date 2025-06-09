11f8c88379e1536d9e2b904f46c784ff5a731670
keshavsharma804 <sharmakeshav364@gmail.com>
2025-06-09 14:56:56 +0530

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -30,6 +30,20 @@ def palindrome():
         print("Its palindrome")
     else:
         print("Its not a palindrome")
+        
+        
+        
+# Check if a list is a palindrome
+def num_palindrome():
+    a = [2,4,6,8]
+    if a == a[::-1]:
+        print("Num is palindrome")
+    else:
+        print("Its not")
+
+if __name__ == "__main__":
+    num_palindrome()
+
 
 
 # Count vowels in a string
@@ -52,7 +66,6 @@ def count_digits(d):
             count += 1
     return count
 d = "ihaveMango456"
-
 
 
 # Reverse a list without .reverse()
@@ -78,8 +91,63 @@ def reverse_string():
     a = "jack_sparrow"
     print("Reversed String:", a[::-1])
     
-if __name__ == "__main__":
-    reverse_string()
-        
+   
 
+# Sum of digits of a number.
+def sum_of_digits():
+    num = input("Enter a number: ")
+    sum = 0
+    for i in num:
+        sum += int(i)
+    print("Sum of digits:", sum)
+
+
+# sum of number
+def sum_of_list(numbers):
+    total_sum = 0
+    for num in numbers:
+        total_sum += num
+    return total_sum
+
+numbers_list = [1,2,3,4,5,6]
+print(sum_of_list(numbers_list))
+
+
+#multiplication of number
+def mul_of_num(num):
+    mul = 1
+    for i in num:
+        mul *= i
+    return mul
+
+num_list = [4,5]
+print(mul_of_num(num_list))
+
+
+
+
+# find a fibonacci
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+# # Input & Output
+num = int(input("Enter the position (starting from 0): "))
+fib_num = fibonacci(num)
+print(f"The Fibonacci number at position {num} is: {fib_num}")
+
+
+
+# find a factorial
+def factorial(n):
+    if n == 0 or n == 1:  # Base case
+        return 1
+    return n * factorial(n - 1)  # Recursive case
+
+# # Test
+num = int(input("Enter a number: "))
+print(f"Factorial of {num} is {factorial(num)}")
 
