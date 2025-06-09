9c00598368d7618e563cabd829f98ded7f3a9d29
keshavsharma804 <sharmakeshav364@gmail.com>
2025-06-09 12:07:19 +0530

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -1,30 +1,36 @@
 # Find if a number is odd or even
 
-a = int(input("Add number:"))
-if a%2 == 0:
-    print("even")
-else:
-    print("odd")
+def odd_or_even():
+    a = int(input("Add number:"))
+    if a%2 == 0:
+       print("even")
+    else:
+        print("odd")
+    
+
 
 
 # Find the largest out of 3 numbers
-a = int(input("Add number:"))
-b = int(input("Add number:"))
-c = int(input("Add number:"))
-if a > b:
-    print(a)
-elif b > c:
-    print(b)
-elif c > a:
-    print(c)
+def Largest_number():
+    a = int(input("Add number:"))
+    b = int(input("Add number:"))
+    c = int(input("Add number:"))
+    if a > b:
+        print(a)
+    elif b > c:
+        print(b)
+    elif c > a:
+        print(c)
+
 
 
 # Check if a string is a palindrome
-a = "jack"
-if a == a[::-1]:
-    print("Its palindrome")
-else:
-    print("Its not a palindrome")
+def palindrome():
+    a = "jack"
+    if a == a[::-1]:
+        print("Its palindrome")
+    else:
+        print("Its not a palindrome")
 
 
 # Count vowels in a string
@@ -36,7 +42,8 @@ def count_vowel(s):
             count += 1
     return count
 s = "Hello World"
-print(count_vowel(s))
+
+
 
 # Count only even digits in a string
 def count_digits(d):
@@ -46,11 +53,18 @@ def count_digits(d):
             count += 1
     return count
 d = "ihaveMango456"
-print(count_digits(d))
+
+
+
 
 
 # Reverse a list without .reverse()
-a = [2, 4, 6, 8, 10]
-print(a[::-1])
+def reverse_list():
+    a = [2, 4, 6, 8, 10]
+    print("Reversed list:", a[::-1])
+
+if __name__ == "__main__":
+    reverse_list()
+
 
 
